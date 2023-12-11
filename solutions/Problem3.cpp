b4c835c2de5b48ba9c207cd431d3633c843f26f4
Basil Rohner <basil.rohner@outlook.de>
2023-12-11 19:00:29 +0100

    Update Problem3.cpp

--- solutions/Problem3.cpp
+++ solutions/Problem3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 // For the problem description refer to:
-// ...
+// https://codeforces.com/problemset/problem/1901/A
 // Rename to main to compile and see that this is the only main fnc:
 
 int main() {
